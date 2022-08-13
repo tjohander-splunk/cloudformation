a87d44ff91a3e59f037eafbc88f253bad75b50ba
Thomas Johander <tjohander@splunk.com>
2022-08-13 10:07:07 -0500

    Refactored structure to bring in Codepipeline & O11y Events

--- cli/cli.sh
+++
@@ -1,3 +0,0 @@
-aws cloudformation describe-stacks --stack-name vpc-stack
-
-aws cloudformation create-stack --stack-name ec2-instances-stack --template-url https://s3.us-west-2.amazonaws.com/cloudformation-templates-us-west-2/EC2InstanceWithSecurityGroupSample.template
